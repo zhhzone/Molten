6211f81fedd69deaea56a41d116a1067c136f2fb
silkcutKs <silkcutbeta@gmail.com>
2017-08-11 09:22:44 +0800

    add 5.4

---
+++ .travis/composer_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+PHP_VERSION=$1
+TEST_PATH=$2
+
+COMPOSER_JSON=$TEST_PATH/composer.json
+OLD_JOSN=$TEST_PATH/composer.json.5.4
+
+function version_ge()
+{
+    python << EOF
+import sys
+from distutils.version import LooseVersion
+
+sys.exit(not (LooseVersion('$1') > LooseVersion('$2')))
+EOF
+}
+
+# change es version by php version
+if version_ge $PHP_VERSION "5.6.0"; then
+    # do nothing
+    echo "skip"
+else
+    cp $OLD_JOSN  $COMPOSER_JSON
+fi
