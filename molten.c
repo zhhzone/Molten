6b6893f10d09c465d560efda453f28c840f97c5a
silkcutKs <silkcutbeta@gmail.com>
2017-08-11 14:26:45 +0800

    add ot

--- molten.c
+++ molten.c
@@ -692,6 +692,7 @@ PHP_MINFO_FUNCTION(molten)
 {
     php_info_print_table_start();
     php_info_print_table_header(2, "molten support", "enabled");
+    php_info_print_table_header(2, "plugin support", "pdo mysqli phpredis memcahced curl mongodb guzzle elasticsearch");
     php_info_print_table_end();
 
     DISPLAY_INI_ENTRIES();
